a6828b9452b8e41fdb0f742e08aa2798ac9efb86
Gonzalo Chacaltana <gchacaltanab@outlook.com>
2019-10-22 00:54:12 -0500

    add Method EstimatedCost Node - And create App Script with Main Method

---
+++ App.py
@@ -0,0 +1,75 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+# App Script
+__author__ = "Gonzalo Chacaltana Buleje"
+__email__ = "gchacaltanab@gmail.com"
+
+import json
+import sys
+from Node import Node
+
+
+class App(object):
+
+    def __init__(self, jsonfile, nodeSearched):
+        self.listNodes = []
+        self.keyChildren = "children_nodes"
+        self.keyEstimatedCost = "estimated_cost"
+        self.parentNode = {}
+        self.jsonfile = jsonfile
+        self.nodeSearched = nodeSearched
+
+    def main(self):
+        self.openfile()
+        self.createNodes()
+        self.showNodes()
+
+    def openfile(self):
+        print("\n1. Abriendo fichero.")
+        with open(self.jsonfile, "r") as f:
+            self.routes = json.load(f)
+
+    def createNodes(self):
+        i = 0
+        print("\n2. Creando nodos")
+        for node in self.routes:
+            i += 1
+            self.parentNode[i] = Node(node)
+            self.assignChildrenNodes(i, node)
+            self.assignEstimatedCost(i, node)
+
+    def assignChildrenNodes(self, i, node):
+        if self.routes[node].get(self.keyChildren) == None:
+            raise Exception(
+                "El nodo %s no cumple con el formato de nodos hijos." % node)
+        else:
+            for child, cost in self.routes[node][self.keyChildren].items():
+                childNode = Node(child)
+                self.parentNode[i].addChild(childNode, cost)
+            self.listNodes.append(self.parentNode[i])
+
+    def assignEstimatedCost(self, i, node):
+        if self.routes[node].get(self.keyEstimatedCost) == None:
+            raise Exception(
+                "El nodo %s no cumple con el formato de costo estimado." % node)
+        else:
+            self.parentNode[i].assignEstimatedCost(
+                self.routes[node][self.keyEstimatedCost])
+
+    def showNodes(self):
+        # Devuelve lista de nodos creados
+        print("\nLista de Nodos")
+        print("********************")
+        for n in self.listNodes:
+            print("Nodo: %s" % n.name)
+            print("Costo Estimado: %d" % n.estimatedCost)
+            print("Nodos Hijos y costo real\n")
+            print(n.getRealCostChildren())
+            print("\n-----------------------\n")
+
+if __name__ == "__main__":
+    try:
+        app = App("routes.json", "F")
+        app.main()
+    except (ValueError, FileNotFoundError, AttributeError) as ex:
+        print(ex)
--- Node.py
+++ Node.py
@@ -7,14 +7,26 @@ __author__ = "Gonzalo Chacaltana Buleje"
 __email__ = "gchacaltanab@gmail.com"
 
 class Node(object):
+
     def __init__(self, name):
         self.childNodes = []
+        self.childNodesRealCost = {}
         self.name = name
+        self.estimatedCost = 0
 
-    def addChild(self,node):
+    def addChild(self,childNode, realCost):
         # Agrega nodo hijo
-        self.childNodes.append(node)
+        self.childNodes.append(childNode)
+        self.childNodesRealCost[childNode.name] = realCost
 
     def getChildrenNodes(self):
-        # Devuelve los nodos hijos del nodo.
+        # Devuelve los nodos hijos del nodo padre
         return self.childNodes
+    
+    def getRealCostChildren(self):
+        # Devuelve el esfuerzo real por cada nodo hijos del nodo padre
+        return self.childNodesRealCost
+    
+    def assignEstimatedCost(self, value):
+        # Asignar costo estimado a nodo.
+        self.estimatedCost = value
